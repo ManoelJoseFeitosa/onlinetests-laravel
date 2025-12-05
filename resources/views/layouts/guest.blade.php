ad438cba8e19f7b74f7c780d2022ac731fbacacd
Manoel Jose Feitosa <manoelbd2012@gmail.com>
2025-12-05 15:32:05 -0300

    atualização da página guest

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'OnlineTests') - MaFe Provas Online</title>
+    <title>@yield('title', 'MaFe Provas Online') - MaFe Provas Online</title>
     <link rel="icon" href="{{ asset('images/favicon.png') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
